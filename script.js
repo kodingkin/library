31059d0cdacb2f6a9b79a80281d34b48797d0edf
kodingkin <wankin527@gmail.com>
2025-04-30 23:39:18 +0900

    add basic js logic

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function Book(title, author, pages, haveRead) {
+const myLibrary = [];
+
+function Book(title, author, pages, haveRead, id) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
@@ -6,9 +8,41 @@ function Book(title, author, pages, haveRead) {
     this.author = author;
     this.pages = pages;
     this.haveRead = haveRead;
-    this.info = function() {
-      const endString = this.haveRead ? "read before" : "not read yet";
-      const string = `${this.title} by ${this.author}, ${this.pages} pages, ${endString}`
-      return string;
-    }
+    this.id = id;
 }
+
+Book.prototype.info = function() {
+    const endString = this.haveRead ? "read before" : "not read yet";
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${endString}`
+}
+
+function addBookToLibrary(myLibrary, title, author, pages, haveRead) {
+    const id = crypto.randomUUID();
+    const newBook = new Book (title, author, pages, haveRead, id);
+    myLibrary.push(newBook);
+    return newBook;
+}
+
+function displayLibrary(myLibrary) {
+    myLibrary.forEach(book => {
+        const container = document.querySelector(".book-container");
+        const bookElm = document.createElement("div");
+        bookElm.classList.add("book");
+        bookElm.textContent = book.info();
+        container.appendChild(bookElm);
+    });
+}
+
+function removeBook(myLibrary, id) {
+    const newLibrary = myLibrary.filter(book => book.id !== id);
+    return newLibrary;
+}
+
+function changeHaveReadStatus(myLibrary, id, haveRead) {
+    const theBookIndex = myLibrary.findIndex(book => book.id === id);
+    myLibrary[theBookIndex].haveRead = haveRead;
+}
+
+const momotaro = addBookToLibrary(myLibrary, "momotaro", "kentaro", 20, true);
+const snowWhite = addBookToLibrary(myLibrary, "snowWhite", "lisa elsa", 400, false);
+displayLibrary(myLibrary);
